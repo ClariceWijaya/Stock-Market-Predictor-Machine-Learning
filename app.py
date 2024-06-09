4b640420e855997523c3b3bf9a3302d51d66b50d
ClariceWijaya <154241321+ClariceWijaya@users.noreply.github.com>
2024-06-10 01:56:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import yfinance as yf
+import tensorflow as tf
 from keras.models import load_model
 import streamlit as st
 from sklearn.preprocessing import MinMaxScaler
