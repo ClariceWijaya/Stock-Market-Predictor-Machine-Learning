b70345a6fb1149dd6518de769d46d2d7130b0bfb
ClariceWijaya <154241321+ClariceWijaya@users.noreply.github.com>
2024-06-10 12:00:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,13 @@ import plotly.graph_objects as go
 import plotly.express as px
 import matplotlib.pyplot as plt
 
-# Load the pre-trained model
-model = load_model('Gold-price-prediction.keras', compile=False)
+# Attempt to load the pre-trained model
+model_loaded = False
+try:
+    model = load_model('Gold-price-prediction.keras', compile=False)
+    model_loaded = True
+except Exception as e:
+    st.error(f"Error loading model: {e}")
 
 st.set_page_config(layout="wide")
 st.title('Stock Market Predictor')
