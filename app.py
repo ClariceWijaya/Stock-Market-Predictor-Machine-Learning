b197baade8740d91175339da83533f6f88511101
ClariceWijaya <154241321+ClariceWijaya@users.noreply.github.com>
2024-06-10 11:53:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.title('Stock Market Predictor')
 
 try:
     # Load the pre-trained model
-    model = load_model('Gold-price-prediction.h5', custom_objects={'CustomLayer': CustomLayer})
+    model = load_model('Gold-price-prediction.h5')
 except Exception as e:
     st.error(f"Error loading model: {e}")
 
